0d9b716744c84f4b1d64b02a960ce195c8a89c4c
DESKTOP-2HRTPPV\Sentiments <qianqian1903@gmail.com>
2023-04-27 23:53:25 +0800

    Added animated toggle for hamburger menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
-import { menu } from "../constants/headerNavList";
+import {  AiOutlineShoppingCart } from "react-icons/ai";
 import { useCartCtx } from "../contexts/CartCtx";
+import { menu } from "../constants/headerNavList";
 import Avatar from "./Avatar";
 import MobileNavMenu from "./MobileNavMenu";
 import useMediaQuery from "../hooks/useMediaQuery";
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const { toggleCart, totalUnits } = useCartCtx();
     const [menuVisible, setMenuVisible] = useState(false);
     const matchMedia = useMediaQuery("(min-width:1024px)");
@@ -17,21 +18,46 @@ const Navbar = () => {
         <>
             <div className="mr-3 hidden items-center gap-3 justify-self-end lg:flex">
                 {menu.map((m) => (
-                    <NavLink
+                    <button
+                        type="button"
                         className="btn-ghost btn-circle btn font-normal"
-                        to={m.link}
+                        // to={m.link}
                         key={m.text}
+                        onClick={() => {
+                            setMenuVisible(false);
+                            navigate(m.link);
+                        }}
                     >
                         {m.text}
-                    </NavLink>
+                    </button>
                 ))}
             </div>
 
             <button
-                className="btn-ghost btn-square btn justify-self-end lg:hidden"
+                type="button"
+                className={`${
+                    menuVisible ? "swap-active" : ""
+                } swap-rotate swap btn-ghost btn-circle btn justify-self-end border-transparent lg:hidden`}
                 onClick={() => setMenuVisible(!menuVisible)}
             >
-                <AiOutlineMenu size="24px" />
+                <svg
+                    className="swap-off fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 512 512"
+                >
+                    <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
+                <svg
+                    className="swap-on fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 512 512"
+                >
+                    <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                </svg>
             </button>
 
             {matchMedia && (
